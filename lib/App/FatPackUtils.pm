016358be4f83ab85fcf52f159fcf79eba58a98fb
perlancar (@pc-office) <perlancar@gmail.com>
2015-04-16 13:27:36 +0700

    Initial commit

---
+++ lib/App/FatPackUtils.pm
@@ -0,0 +1,23 @@
+package App::FatPackUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+
+1;
+# ABSTRACT: Collection of CLI utilities related to App::FatPacker
+
+=head1 DESCRIPTION
+
+This distribution provides the following command-line utilities related to
+L<App::FatPacker>:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<App::FatPacker>, L<App::fatten>
+
+=cut
